591b3cdd18a532b28ece11e0da0d25f002059056
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-06 10:56:28 +0300

    delete getmapping

--- src/main/java/com/gitTestProject/controller/MainController.java
+++ src/main/java/com/gitTestProject/controller/MainController.java
@@ -6,9 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping
-    public String main()
-    {
-        return "main";
-    }
 }
