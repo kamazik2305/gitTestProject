2d009d163183aa984b5642081a844151ba70385b
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-06 14:47:38 +0300

    add putmapping

--- src/main/java/com/gitTestProject/controller/MainController.java
+++ src/main/java/com/gitTestProject/controller/MainController.java
@@ -1,9 +1,6 @@
 package com.gitTestProject.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
@@ -24,5 +21,11 @@ public class MainController {
     public String get()
     {
         return "get";
+    }
+
+    @PutMapping
+    public String puy()
+    {
+        return "put";
     }
 }
