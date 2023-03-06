9e5aa2500d3bd9ac0c70f4b901e515489495d17c
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-06 10:13:04 +0300

    add empty priject

---
+++ src/test/java/com/gitTestProject/GitTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
