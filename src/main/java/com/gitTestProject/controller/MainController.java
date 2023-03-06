f97b951c8d114e230d37b05dfdb6e9b8af6d137b
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-06 12:06:14 +0300

    add delmapping

--- src/main/java/com/gitTestProject/controller/MainController.java
+++ src/main/java/com/gitTestProject/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.gitTestProject.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,11 @@ public class MainController {
     public String main()
     {
         return  "main";
+    }
+
+    @DeleteMapping
+    public String delete()
+    {
+        return "delete";
     }
 }
