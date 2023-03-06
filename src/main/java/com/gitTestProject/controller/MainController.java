78cc771af274319bb939d4cd7d4de625215ab104
kamazik2305 <viktorpohodko96@gmail.com>
2023-03-06 10:59:42 +0300

    add postmapping

--- src/main/java/com/gitTestProject/controller/MainController.java
+++ src/main/java/com/gitTestProject/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.gitTestProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @PostMapping
+    public String main()
+    {
+        return  "main";
+    }
 }
